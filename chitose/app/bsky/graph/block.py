0c2623b996022b8f1a7e49b09534825eec86dc76
Muneyuki Noguchi <nogu.dev@gmail.com>
2023-04-30 08:35:07 +0900

    Add block-related files

---
+++ chitose/app/bsky/graph/block.py
@@ -0,0 +1,12 @@
+# GENERATED CODE - DO NOT MODIFY
+from __future__ import annotations
+import chitose
+
+class Block(chitose.Record):
+
+    def __init__(self, subject: str, created_at: str) -> None:
+        self.subject = subject
+        self.created_at = created_at
+
+    def to_dict(self):
+        return {'subject': self.subject, 'createdAt': self.created_at}
---
+++ chitose/app/bsky/graph/get_blocks.py
@@ -0,0 +1,8 @@
+# GENERATED CODE - DO NOT MODIFY
+from __future__ import annotations
+import chitose
+import typing
+
+def get_blocks(service: str, headers: dict[str, str], limit: typing.Optional[int]=None, cursor: typing.Optional[str]=None):
+    """Who is the requester's account blocking?"""
+    return chitose.xrpc.call('app.bsky.graph.getBlocks', [('limit', limit), ('cursor', cursor)], None, service, {} | headers)
