731ac028c1e697a6a7939f7fb55d95db5071f5b3
Muneyuki Noguchi <nogu.dev@gmail.com>
2023-05-01 08:47:42 +0900

    Use com.atproto.server.create_session()

--- chitose/agent.py
+++ chitose/agent.py
@@ -3,7 +3,6 @@ import json
 
 from .app import App
 from .com import Com
-from chitose.com.atproto.server import create_session
 
 
 class BskyAgent:
@@ -21,8 +20,8 @@ class BskyAgent:
 
     def login(self, identifier: str, password: str) -> None:
         session = json.loads(
-            create_session(service=self.service, headers={},
-                           identifier=identifier, password=password))
+            self.com.atproto.server.create_session(
+                identifier=identifier, password=password))
         if 'accessJwt' in session:
             self.headers['authorization'] \
                 = f'Bearer {session["accessJwt"]}'
