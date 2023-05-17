37728bfac4d64740ec2c9ad709222b5c7b076ae2
Muneyuki Noguchi <nogu.dev@gmail.com>
2023-05-18 08:40:28 +0900

    Switch to PyData

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
 sys.path.insert(0, os.path.abspath('../..'))
