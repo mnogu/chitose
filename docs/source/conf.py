3a439a0da92ad8ba13e2494bea3c31a8f28bc95c
Muneyuki Noguchi <nogu.dev@gmail.com>
2023-05-18 08:04:34 +0900

    Remove the release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,6 @@ import os
 project = 'Chitose'
 copyright = '2023, Muneyuki Noguchi'
 author = 'Muneyuki Noguchi'
-release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
